5791ce71aa7362270ebd71f82f2bdb24a9d91ea7
kaipat <597683167@qq.com>
2022-05-31 11:58:19 +0800

    fix: improve

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 
 import path from 'path';
+import fs from 'fs';
 import got from 'got';
 import { createWriteStream } from 'fs';
 
 
 function get_file_name(url) {
-  return path.basename(url)
+  return path.basename(url);
 } 
 
-export default function(from, to, options) {
-  const readStream = got.stream(from)
+export default function fastload(from, to, options) {
+  try {
+    const stat = fs.statSync(path.resolve(to));
+    if (!stat.isDirectory()) {
+      fs.mkdirSync(to);
+    }
+  } catch(err) {
+    fs.mkdirSync(to); 
+  }
+  const readStream = got.stream(from);
   readStream.on('response', response => {
     readStream.pipe(
       createWriteStream(
         path.resolve(to, get_file_name(from))
       )
     )
-  })
+  });
 }
 
 
--- test.js
+++ test.js
@@ -1,5 +1,11 @@
+// import fastload from 'fastload';
 
-import fastload from 'fastload';
+// fastload(
+//   'https://bing.oneneko.com/image/BrownHares_ZH-CN6625339934_1920x1080.jpg', 
+//   'public'
+// );
+
+import fastload from './index.js';
 
 fastload(
   'https://bing.oneneko.com/image/BrownHares_ZH-CN6625339934_1920x1080.jpg', 
